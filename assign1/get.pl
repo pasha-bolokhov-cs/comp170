c6867301aae799a19f517d555b01d6223fd82e13
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-01-07 12:45:56 -0800

    Initial revision

---
+++ assign1/get.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w 
+
+use IO::Socket;
+
+unless (@ARGV > 1) { die "usage: $0 host document ..." }
+$host = shift(@ARGV);
+$EOL = "\015\012";
+$BLANK = $EOL x 2;
+ 
+foreach $document ( @ARGV ) {
+    $remote = IO::Socket::INET->new( Proto => "tcp",
+				     PeerAddr => $host,
+				     PeerPort => "http(80)",);
+    unless ($remote) { die "cannot connect to http daemon on $host" }
+    $remote->autoflush(1);
+    print $remote "GET $document HTTP/1.1" . $BLANK;
+    while ( <$remote> ) { print }
+    close $remote;
+}
