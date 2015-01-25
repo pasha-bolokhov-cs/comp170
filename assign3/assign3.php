4e22fb8eb2ebe5d8a80f43e16c1a7741664219b4
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-01-24 20:44:49 -0800

    Add AngularJS and Bootstrap and JQuery

--- assign3/assign3.php
+++ assign3/assign3.php
@@ -11,6 +11,15 @@
 -->
 <?php
 	$record = trim(fgets(STDIN), "\n");
-	preg_match_all('^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s[^"]*"[^"]*"\s(\d+)\s([0-9\-])\s', $record, $matches, PREG_PATTERN_ORDER);
-	print_r($matches);
+	preg_match_all('/^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s[^"]*"[^"]*"\s(\d+)\s([0-9\-]+)\s/',
+		       $record, $matches, PREG_PATTERN_ORDER);
+	$ip = $matches[1][0];
+	$status = $matches[2][0];
+	$size = $matches[3][0];
+	echo "\n";
+	echo "ip = `$ip', status = $status, size = $size\n";
+	/* check if the request was not successful (success = 2xx) */
+	if ($status[0] != '2') {
+		$size = 0;
+	}
 ?>
