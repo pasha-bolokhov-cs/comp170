6eec72e1b04df8469b4f983beab60f257cdb4374
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-01-25 01:56:50 -0800

    Edit more

--- assign3/parse-log.php
+++ assign3/parse-log.php
@@ -1,4 +1,5 @@
-<!--
+<?php
+/*
   Assignment 3
   Program name: ##
   Author: Pasha Bolokhov <pasha.bolokhov@gmail.com>
@@ -8,18 +9,29 @@
   Description: ##
   Invocation: ##
   Requires: ##
--->
-<?php
-	$record = trim(fgets(STDIN), "\n");
+*/
+	/* get the log record from JSON data */
+	$jsonData = file_get_contents("php://input");
+	$data = json_decode($jsonData);
+	$record = $data->record;
+
+	/* validate the record */
+	$record = strip_tags(trim($record));
+
+	/* parse the IP address, the status and the response size from the record */
 	preg_match_all('/^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s[^"]*"[^"]*"\s(\d+)\s([0-9\-]+)\s/',
 		       $record, $matches, PREG_PATTERN_ORDER);
 	$ip = $matches[1][0];
 	$status = $matches[2][0];
 	$size = $matches[3][0];
-	echo "\n";
-	echo "ip = `$ip', status = $status, size = $size\n";
+
 	/* check if the request was not successful (success = 2xx) */
 	if ($status[0] != '2') {
-		$size = 0;
+		$size = 0;		// Unsuccessful requests default to zero size
 	}
+
+	/* return the response */
+	$response["ip"] = $ip;
+	$response["size"] = $size;
+	echo json_encode($response);
 ?>
