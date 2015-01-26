c212b959c50e9e46c2de60b6c9a4f3f6345fe362
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-01-25 16:23:26 -0800

    Edit more

--- assign3/reg.php
+++ assign3/reg.php
@@ -1,3 +1,18 @@
+<!--
+  Assignment 3
+  Program name: reg.php
+  Author: 
+  Date: January 24, 2015
+  Estimated Completion Time: 6 hr
+  Actual Completion Time: 12 hr 
+  Description: 
+	A program that takes a pattern and a string from command line arguments
+	and prints out all matches of the pattern in the string
+  Invocation: 
+	$ php reg.php <pattern> <string>
+  Requires: n/a
+-->
+
 <?php
 	/* obtain the pattern and the string from command line arguments */
 	$regex = $argv[1];
