d783e5bbda32e7f4d05e09b9d4cbff47a718e0dc
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-01-25 16:13:27 -0800

    Edit more

--- assign3/reg.php
+++ assign3/reg.php
@@ -1,6 +1,11 @@
 <?php
+	/* obtain the pattern and the string from command line arguments */
 	$regex = $argv[1];
 	$string = $argv[2];
+
+	/* find the matches */
 	preg_match_all('`' . $regex . '`', $string, $matches);
+
+	/* print the matches */
 	print_r($matches);
 ?>
