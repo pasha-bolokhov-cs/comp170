1ff49e417c73499c8786cb1615222f02d8baabc3
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-02-10 10:20:24 -0800

    Edit more

--- assign5/three-query.php
+++ assign5/three-query.php
@@ -77,6 +77,14 @@ SELECT e.last_name AS "Last Name",
        ON (d.location_id = l.location_id)
        WHERE UPPER(l.city) LIKE 'SOUTHLAKE';
 EOF_QUERY0;
+$title[0] = "Who works in Southlake";
+$table_head[0] = <<<"HEAD0"
+  			<th>Last Name</th>
+  			<th>Job</th>
+  			<th>Department No</th>
+  			<th>Department</th>
+HEAD0;
+
 
 $query[1] = <<<"EOF_QUERY1"
 SELECT e.last_name AS "Last Name",
@@ -85,6 +93,7 @@ SELECT e.last_name AS "Last Name",
        USING (department_id)
        WHERE UPPER(e.last_name) LIKE 'G%';
 EOF_QUERY1;
+$title[1] = "Whose name starts with `G'";
 
 $query[2] = <<<"EOF_QUERY2"
 SELECT e.last_name AS "Employee",
@@ -95,50 +104,63 @@ SELECT e.last_name AS "Employee",
        ON (e.manager_id = m.employee_id)
        WHERE UPPER(e.last_name) LIKE 'T%';
 EOF_QUERY2;
+$title[2] = "Bosses of those whose names start with `G'";
 
-/* do the query */
-if (($result = $mysqli->query($query)) === FALSE) {
-	$error = 'Query Error (' . $mysqli->error . ') ';
-	$mysqli->close();
-	goto got_error;
-}
+for ($i = 0; $i < count($query); $i++) {
+	/* do the query */
+	if (($result = $mysqli->query($query[$i])) === FALSE) {
+		$error = 'Query Error (' . $mysqli->error . ') ';
+		$mysqli->close();
+		goto got_error;
+	}
 ?>
 	<div class="row">
 	  <div class="col-xs-12 col-md-8">
 	    <div class="panel panel-info">
   	      <div class="panel-heading">
-  		Query ###
+<?php echo $title[$i];?>
 	      </div>
+	      <!-- Table contents depends on $i -->
               <table class="table">
   	        <thead>
   		  <tr>
-  			<th>Last Name</th>
-  			<th>Job</th>
-  			<th>Department No</th>
-  			<th>Department</th>
+<?php
+	switch($i) {
+	case 0:
+		break;
+
+	case 1:
+		echo <<<"HEAD1"
+HEAD1;
+		break;
+
+	case 2:
+?>
   		  </tr>
   	        </thead>
   	        <tbody>
 <?php
-/* print the results */
-while ($row = $result->fetch_assoc()) {
-	echo <<<"TAB_END1"
-<tr>
-	<td> {$row["Last Name"]} </td>
-	<td> {$row["Job"]} </td>
-	<td> {$row["Dept No"]} </td>
-	<td> {$row["Department"]} </td>
-</tr>
-TAB_END1;
-}
+	/* print the results */
+	while ($row = $result->fetch_assoc()) {
+		echo <<<"TAB_END0"
+	<tr>
+		<td> {$row["Last Name"]} </td>
+		<td> {$row["Job"]} </td>
+		<td> {$row["Dept No"]} </td>
+		<td> {$row["Department"]} </td>
+	</tr>
+TAB_END0;
+	}
 ?>
 	        </tbody>
               </table>
 	    </div> <!-- /panel -->
           </div> <!-- /column -->
-	</div>
+	</div> <!-- /row -->
 
 <?php
+} // for ($i)
+
 /* close connection and leave */
 $mysqli->close();
 goto quit;
