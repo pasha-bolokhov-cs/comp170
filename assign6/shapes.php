663fd48ca19a8d3a18c5fada1b7f8f4714f31890
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-02-16 20:59:13 -0800

    Edit more

--- assign6/shapes.php
+++ assign6/shapes.php
@@ -11,9 +11,23 @@
   Requires: ##
 */
 
+/*
+ * Converts up-growing 'y' coordinate to canvas 'y' coordinate
+ */
+function convert_y($y, $cheight) {
+	return $cheight - $y - 1;
+}
+
 function draw_square($res, $cwidth, $cheight, $fgcolor, $bgcolor, $loc_x, $loc_y, $size) {
-	imagefilledrectangle($res, $loc_x, $cheight - ($loc_y + $size), $loc_x + $size - 1, $cheight - $loc_y - 1, $bgcolor);
-	imagerectangle($res, $loc_x, $cheight - ($loc_y + $size), $loc_x + $size - 1, $cheight - $loc_y - 1, $fgcolor);
+	imagefilledrectangle($res, $loc_x, convert_y($loc_y + $size - 1, $cheight),
+			     $loc_x + $size - 1, convert_y($loc_y, $cheight), $bgcolor);
+	imagerectangle($res, $loc_x, convert_y($loc_y + $size - 1, $cheight),
+		       $loc_x + $size - 1, convert_y($loc_y, $cheight), $fgcolor);
+}
+
+function draw_circle($res, $cwidth, $cheight, $fgcolor, $bgcolor, $loc_x, $loc_y, $rad) {
+	imagefilledellipse($res, $loc_x, convert_y($loc_y, $cheight), 2 * $rad, 2 * $rad, $bgcolor);
+	imageellipse($res, $loc_x, convert_y($loc_y, $cheight), 2 * $rad, 2 * $rad, $fgcolor);
 }
 
 ?>
