58c22525d883f65e98b936469a5d74f3a6a82919
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-02-17 19:59:07 -0800

    Split image into parts

--- assign6/assign6.php
+++ assign6/assign6.php
@@ -20,42 +20,57 @@ require_once("shapes.php");
 /* Get the shape variable */
 $shape = @$_GET["shape"] or die("Need parameter `shape'");
 
-$width = 400;
-$height = 160;
-
-/* 
- * Get rid of numbers in the end of "shape"
- * These numbers can be added so that
- * the same shape is accessed via different URLs,
- * which, in turn, is needed so that the
- * image with the shape does not get cached
- */
-$shape = preg_replace('/\d*/', "", $shape);
+$width = 410;
+$height = 346;
 
 /* call a function depending on parameter */
 switch ($shape) {
-case "square":
+case "square1":
 	$size = rand(50, $height / 2);
 	draw_square($width, $height,
 	    	    random_color(), random_color(),
 	    	    rand(10, $width - 10 - $size), rand(10, $height - 10 - $size), $size,
-		    "img/sunset.jpg");
+		    "img/sunset_11.jpg");
 	break;
 
-case "circle":
+case "square2":
+	$size = rand(50, $height / 2);
+	draw_square($width, $height,
+	    	    random_color(), random_color(),
+	    	    rand(10, $width - 10 - $size), rand(10, $height - 10 - $size), $size,
+		    "img/sunset_21.jpg");
+	break;
+
+case "circle1":
 	$rad = rand(50, $height / 4);
 	draw_circle($width, $height,
 		    random_color(), random_color(),
 		    rand($rad + 10, $width - 10 - $rad), rand($rad + 10, $height - 10 - $rad), $rad,
-		    "img/sunset.jpg");
+		    "img/sunset_12.jpg");
 	break;
 
-case "triangle":
+case "circle2":
+	$rad = rand(50, $height / 4);
+	draw_circle($width, $height,
+		    random_color(), random_color(),
+		    rand($rad + 10, $width - 10 - $rad), rand($rad + 10, $height - 10 - $rad), $rad,
+		    "img/sunset_22.jpg");
+	break;
+
+case "triangle1":
 	$side = rand(80, $height / 2);
 	draw_tri($width, $height,
 		 random_color(), random_color(),
 		 rand($side / 2 + 10, $width - 10 - $side / 2), rand($side / 2 + 5, $height - 10 - $side / 2), $side,
-		"img/sunset.jpg");
+		"img/sunset_13.jpg");
+	break;
+
+case "triangle2":
+	$side = rand(80, $height / 2);
+	draw_tri($width, $height,
+		 random_color(), random_color(),
+		 rand($side / 2 + 10, $width - 10 - $side / 2), rand($side / 2 + 5, $height - 10 - $side / 2), $side,
+		"img/sunset_23.jpg");
 	break;
 
 default:
