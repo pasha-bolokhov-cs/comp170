429054fe05732c038a74d6af4d6e6a77f251ae59
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-02-17 19:23:14 -0800

    Now make all functions create their own images

--- assign6/assign6.php
+++ assign6/assign6.php
@@ -17,48 +17,49 @@
 
 require_once("shapes.php");
 
-/* put the content header */
-header("Content-type: image/png");
+/* Get the shape variable */
+$shape = @$_GET["shape"] or die("Need parameter `shape'");
 
-/* load the background */
-$bgim = @imagecreatefromjpeg("img/sunset.jpg") or
-	die("Could not open background image");
-$width = imagesx($bgim);
-$height = imagesy($bgim);
+$width = 400;
+$height = 160;
 
-/* create the canvas */
-$im = @imagecreatetruecolor($width, $height) or
-      die("Could not create image");
+/* 
+ * Get rid of numbers in the end of "shape"
+ * These numbers can be added so that
+ * the same shape is accessed via different URLs,
+ * which, in turn, is needed so that the
+ * image with the shape does not get cached
+ */
+$shape = preg_replace('/\d*/', "", $shape);
 
-/* draw some shapes */
-for ($i = 0; $i < 2; $i++) {
-	/* square */
+/* call a function depending on parameter */
+switch ($shape) {
+case "square":
 	$size = rand(50, $height / 2);
-	draw_square($im, $width, $height,
-	    	    random_color($im), random_color($im),
-	    	    rand(10, $width - 10 - $size), rand(10, $height - 10 - $size), $size);
+	draw_square($width, $height,
+	    	    random_color(), random_color(),
+	    	    rand(10, $width - 10 - $size), rand(10, $height - 10 - $size), $size,
+		    "img/sunset.jpg");
+	break;
 
-	/* circle */
+case "circle":
 	$rad = rand(50, $height / 4);
-	draw_circle($im, $width, $height,
-		    random_color($im), random_color($im),
-		    rand($rad + 10, $width - 10 - $rad), rand($rad + 10, $height - 10 - $rad), $rad);
+	draw_circle($width, $height,
+		    random_color(), random_color(),
+		    rand($rad + 10, $width - 10 - $rad), rand($rad + 10, $height - 10 - $rad), $rad,
+		    "img/sunset.jpg");
+	break;
 
-	/* triangle */
+case "triangle":
 	$side = rand(80, $height / 2);
-	draw_tri($im, $width, $height,
-		 random_color($im), random_color($im),
-		 rand($side / 2 + 10, $width - 10 - $side / 2), rand($side / 2 + 5, $height - 10 - $side / 2), $side);
+	draw_tri($width, $height,
+		 random_color(), random_color(),
+		 rand($side / 2 + 10, $width - 10 - $side / 2), rand($side / 2 + 5, $height - 10 - $side / 2), $side,
+		"img/sunset.jpg");
+	break;
+
+default:
+	die("Unknown form `$shape'");
 }
 
-/* add our canvas on top of background */
-imagecopymerge($bgim, $im, 0, 0, 0, 0, $width, $height, 20);
-
-/* flush the image */
-imagesavealpha($bgim, TRUE);
-imagepng($bgim);
-
-/* finish up */
-imagedestroy($im);
-imagedestroy($bgim);
 ?>
--- assign6/shapes.php
+++ assign6/shapes.php
@@ -23,44 +23,120 @@ function convert_y($y, $cheight) {
 /*
  * Creates a random colour with a tad of transparency
  */
-function random_color($res) {
-	return imagecolorallocatealpha($res, rand(0, 255), rand(0, 255), rand(0, 255), 16);
+function random_color() {
+	return		 16 * 0x01000000 +	// alpha
+	       rand(0, 255) * 0x00010000 +	// red
+	       rand(0, 255) * 0x00000100 +	// green
+	       rand(0, 255) * 0x00000001;	// blue
+	/* could just have used 'rand(0, 0xFFFFFF)' */
 }
 
 /*
  * Draw a square
  */
-function draw_square($res, $cwidth, $cheight, $fgcolor, $bgcolor, $loc_x, $loc_y, $size) {
-	imagefilledrectangle($res, $loc_x, convert_y($loc_y + $size - 1, $cheight),
+function draw_square($cwidth, $cheight, $fgcolor, $bgcolor, $loc_x, $loc_y, $size, $bgimage) {
+	/* put the content header */
+	header("Content-type: image/png");
+	
+	/* load the background */
+	$bgim = @imagecreatefromjpeg($bgimage) or
+		die("Could not open background image");
+
+	/* create the canvas */
+	$im = @imagecreatetruecolor($cwidth, $cheight) or
+	      die("Could not create image");
+
+	/* draw the rectangle */
+	imagefilledrectangle($im, $loc_x, convert_y($loc_y + $size - 1, $cheight),
 			     $loc_x + $size - 1, convert_y($loc_y, $cheight), $bgcolor);
-	imagerectangle($res, $loc_x, convert_y($loc_y + $size - 1, $cheight),
+	imagerectangle($im, $loc_x, convert_y($loc_y + $size - 1, $cheight),
 		       $loc_x + $size - 1, convert_y($loc_y, $cheight), $fgcolor);
+
+	/* add the background */
+	imagecopymerge($im, $bgim, 0, 0, 0, 0, $cwidth, $cheight, 80);
+	
+	/* flush the image */
+	imagesavealpha($im, TRUE);
+	imagepng($im);
+	
+	/* finish up */
+	imagedestroy($im);
+	imagedestroy($bgim);
 }
 
 /*
  * Draw a circle
  */
-function draw_circle($res, $cwidth, $cheight, $fgcolor, $bgcolor, $loc_x, $loc_y, $rad) {
-	imagefilledellipse($res, $loc_x, convert_y($loc_y, $cheight), 2 * $rad, 2 * $rad, $bgcolor);
-	imageellipse($res, $loc_x, convert_y($loc_y, $cheight), 2 * $rad, 2 * $rad, $fgcolor);
+function draw_circle($cwidth, $cheight, $fgcolor, $bgcolor, $loc_x, $loc_y, $rad, $bgimage) {
+	/* put the content header */
+	header("Content-type: image/png");
+	
+	/* load the background */
+	$bgim = @imagecreatefromjpeg($bgimage) or
+		die("Could not open background image");
+	$width = imagesx($bgim);
+	$height = imagesy($bgim);
+	
+	/* create the canvas */
+	$im = @imagecreatetruecolor($cwidth, $cheight) or
+	      die("Could not create image");
+
+	/* draw the circle */
+	imagefilledellipse($im, $loc_x, convert_y($loc_y, $cheight), 2 * $rad, 2 * $rad, $bgcolor);
+	imageellipse($im, $loc_x, convert_y($loc_y, $cheight), 2 * $rad, 2 * $rad, $fgcolor);
+
+	/* add the background */
+	imagecopymerge($im, $bgim, 0, 0, 0, 0, $cwidth, $cheight, 80);
+	
+	/* flush the image */
+	imagesavealpha($im, TRUE);
+	imagepng($im);
+	
+	/* finish up */
+	imagedestroy($im);
+	imagedestroy($bgim);
 }
 
 /*
  * Draw a triangle
  */
-function draw_tri($res, $cwidth, $cheight, $fgcolor, $bgcolor, $loc_x, $loc_y, $side) {
+function draw_tri($cwidth, $cheight, $fgcolor, $bgcolor, $loc_x, $loc_y, $side, $bgimage) {
+	/* put the content header */
+	header("Content-type: image/png");
+	
+	/* load the background */
+	$bgim = @imagecreatefromjpeg($bgimage) or
+		die("Could not open background image");
+
+	/* create the canvas */
+	$im = @imagecreatetruecolor($cwidth, $cheight) or
+	      die("Could not create image");
+
+	/* draw the triangle */
 	$height = round($side * sqrt(3.0) / 2.0);
-	imagefilledpolygon($res, 
+	imagefilledpolygon($im,
 			   array(
 			  	 $loc_x - $side / 2, convert_y($loc_y - $height / 2, $cheight),
 				 $loc_x, convert_y($loc_y + $height / 2, $cheight),
 				 $loc_x + $side / 2, convert_y($loc_y - $height / 2, $cheight)
 			   ), 3, $bgcolor);
-	imagepolygon($res, 
+	imagepolygon($im, 
 		     array(
 		  	   $loc_x - $side / 2, convert_y($loc_y - $height / 2, $cheight),
 			   $loc_x, convert_y($loc_y + $height / 2, $cheight),
 			   $loc_x + $side / 2, convert_y($loc_y - $height / 2, $cheight)
 			  ), 3, $fgcolor);
+
+	/* add the background */
+	imagecopymerge($im, $bgim, 0, 0, 0, 0, $cwidth, $cheight, 80);
+	
+	/* flush the image */
+	imagesavealpha($im, TRUE);
+	imagepng($im);
+	
+	/* finish up */
+	imagedestroy($im);
+	imagedestroy($bgim);
 }
+
 ?>
